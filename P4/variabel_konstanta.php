68d09c9dd9c9d0ca5e7ecadbdbc7efc05d4c469a
gaduhprakoso <gaduhprakoso@gmail.com>
2025-09-22 07:38:34 +0700

    P4_praktikum1

---
+++ P4/variabel_konstanta.php
@@ -0,0 +1,16 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "hasil penjumlahan $angka1 dan $angka2 adalah $hasil.<br>";
+
+$benar = true;
+$salah = false;
+echo "Variabel benar: $benar, variabel salah: $salah<br>";
+
+define("NAMA_SITUS", "websiteku.com");
+define("TAHUN_PEDRIRIAN", 2023);
+
+echo "selamat datang di" . NAMA_SITUS . ", situs 
+yang didirikan pada tahun" . TAHUN_PEDRIRIAN . ".";
+?>
