5ba6f55f065c0a791799a7b9438f494ca5e977b8
gaduhprakoso <gaduhprakoso@gmail.com>
2026-02-24 10:31:46 +0700

    pwl_jobsheet2

--- Pemrograman_Website_Lanjut/PWL_2026/routes/web.php
+++ Pemrograman_Website_Lanjut/PWL_2026/routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,48 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('selamat datang');
 });
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/world', function () {
+ return 'World';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 244107020150 <br> Nama: Gaduh Prakoso'; 
+});
+
+Route::get('/user/{gaduh}', function ($name) {
+return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{gaduh}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{gaduh}', function ($name='John') {
+return 'Nama saya '.$name;
+});
+
+Route::resource('photos', PhotoController::class)->only([
+ 'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+ 'create', 'store', 'update', 'destroy'
+]);
+
+Route::get('/greeting', [WelcomeController::class,
+'greeting']);
+
+
